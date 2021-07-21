c13505ff9604066746b9c52e99dae4b3d08a96d8
Kristján Oddsson <koddsson@gmail.com>
2021-07-21 13:59:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Install my editor and IDE
-apt-get install neovim
+apt-get install -y neovim
 
 # Update node to a decent version
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
@@ -9,9 +9,11 @@ apt-get install -y nodejs
 npm install -g npm@latest
 
 # Install npm packages
-npm install -g \
-  n                        \ # node version manager
-  @koddsson/coworking-with   # utility tool to handle co-singing of commits
+
+# n: node version manager that I like
+# @koddsson/coworking-with: utility to add `coworking-with` stanzas to commits
+
+npm install -g n @koddsson/coworking-with  
   
 # Start using latest node version
 n latest
