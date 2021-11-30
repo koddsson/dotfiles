d99d4ab0192af009d8cdf4416b99f2ad7eb92b9c
Kristján Oddsson <koddsson@gmail.com>
2021-11-30 22:54:54 +0000

    Install node 16 with sudo

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Update node to a decent version
-curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo bash -
 sudo apt-get install -y nodejs
 
 # Update npm
